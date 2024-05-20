2e6bd539eb15504fd907b31f0190a24e374a89a0
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2024-05-20 13:14:07 +0200

    add helper funcion to read json files

--- server/server.js
+++ server/server.js
@@ -7,3 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
+
+// Helper function to read JSON files
+const readJsonFile = (filePath) => {
+  return JSON.parse(fs.readFileSync(filePath, "utf8"));
+};
